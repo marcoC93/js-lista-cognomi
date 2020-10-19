63fafb9eb02bde5268d2fee9a1f40ecdcd6b8908
Marco <marco.chianese93@gmail.com>
2020-10-19 17:14:16 +0200

    regole trovate ed applicate

---
+++ script.js
@@ -0,0 +1,9 @@
+var cognomi = ["Magro ", "De Vito ", "Sciortino" , "Crimi", "Lombardo", "De santis" ] ;
+//var cognome_utente = prompt('inserisci il tuo cognome') ;
+//cognomi.push(cognome_utente) ;
+
+console.log(cognomi.sort());
+
+var prova = cognomi.indexOf("De Vito") ;
+
+console.log(prova);
